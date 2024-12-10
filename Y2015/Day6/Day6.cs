04ddb75e1b4408d1e14b65cde6fb77202f6f1a2d
Zbyněk Židlický <zbynekzidlicky@icloud.com>
2024-12-10 19:20:32 +0100

    Day 6 done

--- Y2015/Day6/Day6.cs
+++ Y2015/Day6/Day6.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode.Shared;
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.Shared;
 using AdventOfCode.Shared.Years;
 
 namespace AdventOfCode.Y2015.Day6;
@@ -9,7 +10,7 @@ public class Day6(): Year2015(nameof(Day6))
     {
         int totalLights = 0;
         
-        int[,] grid = new int[999,999];
+        int[,] grid = new int[1000,1000];
 
         for (int y = 0; y < grid.GetLength(1); y++)
         {
@@ -19,22 +20,158 @@ public class Day6(): Year2015(nameof(Day6))
             }    
         }
         
-        string input = ReadPlainInput(true);
+        string input = ReadPlainInput();
         string[] lines = input.SplitInput();
 
-        string instructionRegex = @"(turn (on|off) \d+,\d+)|(through \d+,\d+)|(toggle \d+,\d+)";
+        string instructionsPattern = @"(turn (on|off) \d+,\d+)|(through \d+,\d+)|(toggle \d+,\d+)";
 
         foreach (var line in lines)
         {
-            
+            MatchCollection instructions = Regex.Matches(line, instructionsPattern);
+
+            int startX = 0;
+            int startY = 0;
+            int endX = 0;
+            int endY = 0;
+
+            string primaryInstruction = "";
+
+            foreach (Match instruction in instructions)
+            {
+                string instr = Regex.Match(instruction.Value, @"(turn on|turn off|toggle|through)").Value;
+                int[] positions = Regex.Match(instruction.Value, @"(\d+,\d+)").Value.Split(',').Select(int.Parse).ToArray();
+
+                int x = positions[0];
+                int y = positions[1];
+
+                if (instr == "through")
+                {
+                    endX = x;
+                    endY = y;
+                }
+                else
+                {
+                    startX = x;
+                    startY = y;
+
+                    primaryInstruction = instr;
+                }
+            }
+
+            for (int y = startY; y <= endY; y++)
+            {
+                for (int x = startX; x <= endX; x++)
+                {
+                    if (primaryInstruction == "turn on")
+                    {
+                        grid[x, y] = 1;
+                    }
+                    else if (primaryInstruction == "turn off")
+                    {
+                        grid[x, y] = 0;
+                    }
+                    else if (primaryInstruction == "toggle")
+                    {
+                        grid[x, y] = grid[x, y] == 1 ? 0 : 1;
+                    }
+                }
+            }
         }
-        
+
+        for (int y = 0; y < grid.GetLength(1); y++)
+        {
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                if (grid[x, y] == 1)
+                {
+                    totalLights++;
+                }
+            }
+        }
         
         return totalLights.ToString();
     }
-
+    
     protected override string PartTwo()
     {
-        return "";
+        int totalBrightness = 0;
+        
+        int[,] grid = new int[1000,1000];
+
+        for (int y = 0; y < grid.GetLength(1); y++)
+        {
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                grid[x, y] = 0;
+            }    
+        }
+        
+        string input = ReadPlainInput();
+        string[] lines = input.SplitInput();
+
+        string instructionsPattern = @"(turn (on|off) \d+,\d+)|(through \d+,\d+)|(toggle \d+,\d+)";
+
+        foreach (var line in lines)
+        {
+            MatchCollection instructions = Regex.Matches(line, instructionsPattern);
+
+            int startX = 0;
+            int startY = 0;
+            int endX = 0;
+            int endY = 0;
+
+            string primaryInstruction = "";
+
+            foreach (Match instruction in instructions)
+            {
+                string instr = Regex.Match(instruction.Value, @"(turn on|turn off|toggle|through)").Value;
+                int[] positions = Regex.Match(instruction.Value, @"(\d+,\d+)").Value.Split(',').Select(int.Parse).ToArray();
+
+                int x = positions[0];
+                int y = positions[1];
+
+                if (instr == "through")
+                {
+                    endX = x;
+                    endY = y;
+                }
+                else
+                {
+                    startX = x;
+                    startY = y;
+
+                    primaryInstruction = instr;
+                }
+            }
+
+            for (int y = startY; y <= endY; y++)
+            {
+                for (int x = startX; x <= endX; x++)
+                {
+                    if (primaryInstruction == "turn on")
+                    {
+                        grid[x, y] += 1;
+                    }
+                    else if (primaryInstruction == "turn off")
+                    {
+                        grid[x, y] -= grid[x, y] > 0 ? 1 : 0;
+                    }
+                    else if (primaryInstruction == "toggle")
+                    {
+                        grid[x, y] += 2;
+                    }
+                }
+            }
+        }
+
+        for (int y = 0; y < grid.GetLength(1); y++)
+        {
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                totalBrightness += grid[x, y];
+            }
+        }
+        
+        return totalBrightness.ToString();
     }
 }
--- obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea8ee0cf35679516a458b2f724118846d31941ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29556d40d23890daca51f66224ef474c366922b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
