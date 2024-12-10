29556d40d23890daca51f66224ef474c366922b0
Zbyněk Židlický <zbynekzidlicky@icloud.com>
2024-12-10 12:02:21 +0100

    day 6 start

--- Y2015/Day6/Day6.cs
+++ Y2015/Day6/Day6.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode.Shared.Years;
+﻿using AdventOfCode.Shared;
+using AdventOfCode.Shared.Years;
 
 namespace AdventOfCode.Y2015.Day6;
 
@@ -6,7 +7,30 @@ public class Day6(): Year2015(nameof(Day6))
 {
     protected override string PartOne()
     {
-        return "";
+        int totalLights = 0;
+        
+        int[,] grid = new int[999,999];
+
+        for (int y = 0; y < grid.GetLength(1); y++)
+        {
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                grid[x, y] = 0;
+            }    
+        }
+        
+        string input = ReadPlainInput(true);
+        string[] lines = input.SplitInput();
+
+        string instructionRegex = @"(turn (on|off) \d+,\d+)|(through \d+,\d+)|(toggle \d+,\d+)";
+
+        foreach (var line in lines)
+        {
+            
+        }
+        
+        
+        return totalLights.ToString();
     }
 
     protected override string PartTwo()
